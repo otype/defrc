163167cd1a0ac5bd547eedffe3314cc291912991
Hans-Gunther Schmidt <hans@otype.de>
2009-12-21 18:00:55 +0200

    Had to remove rescue clauses in each collector

--- lib/collectors/CLDumpsysUsagestats.rb
+++ lib/collectors/CLDumpsysUsagestats.rb
@@ -25,11 +25,7 @@ class CLDumpsysUsagestats < CollectorModule
   end
   
   def self.run
-    begin
-      (process.merge({ "TIME" => "#{formatted_time}" })).inspect.gsub("=>",":")
-    rescue Exception -> msg
-      log_error(msg)      
-    end
+    (process.merge({ "TIME" => "#{formatted_time}" })).inspect.gsub("=>",":")
   end
   
   private
